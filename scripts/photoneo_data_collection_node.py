d2d93ab0217ab413a10c0817e9311fa20a9d1f44
Michael Ripperger <michael.ripperger@swri.org>
2024-02-14 11:45:36 -0600

    Adapted data collection node for Photoneo

--- scripts/photoneo_data_collection_node.py
+++ scripts/photoneo_data_collection_node.py
@@ -4,10 +4,14 @@ from cv_bridge import CvBridge
 import cv2
 import datetime as dt
 from geometry_msgs.msg import TransformStamped
+from message_filters import ApproximateTimeSynchronizer, Subscriber
 import numpy as np
 import os
+import open3d as o3d
+from phoxi_camera.srv import *
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, PointCloud2
+import sensor_msgs.point_cloud2 as pc2
 from std_srvs.srv import *
 from tf2_ros import TransformListener, Buffer
 import yaml
@@ -22,60 +26,74 @@ class DataCollector:
 
         self.img_subdir = 'images'
         self.pose_subdir = 'poses'
+        self.cloud_subdir = 'clouds'
 
         self.poses = []
         self.images = []
+        self.point_clouds = []
 
         # Set up tf listener
         self.buffer = Buffer()
         self.listener = TransformListener(self.buffer)
 
         # Set up image subscriber
-        self.last_frame : Image = None
         self.cvb = CvBridge()
-        self.img_sub = rospy.Subscriber('image', Image, callback=self.img_cb)
+        self.img_sub = Subscriber('image', Image)
+        self.pc_sub = Subscriber('cloud', PointCloud2)
+        self.ts = ApproximateTimeSynchronizer([self.img_sub, self.pc_sub], 10, self.sync_time, allow_headerless=True)
+        self.ts.registerCallback(self.sync_cb)
 
         # Set up servers
         self.collect_server = rospy.Service('collect', Trigger, self.collect_cb, 1)
         self.save_server = rospy.Service('save', Trigger, self.save_cb, 1)
 
-    
-    def img_cb(self, img_msg : Image) -> None:
-        self.last_frame = img_msg
-    
+    def sync_cb(self,
+                img_msg: Image,
+                pc_msg: PointCloud2) -> None:
+        try:
+            rospy.loginfo('Acquiring data...')
+            # Collect pose
+            pose = self.buffer.lookup_transform(self.base_frame, self.tool_frame, img_msg.header.stamp, rospy.Duration(1.0))
+
+            # Save most recent Image
+            image = self.cvb.imgmsg_to_cv2(img_msg)
+            if image.dtype != np.dtype(np.uint8):
+                image = cv2.normalize(image, cv2.NORM_MINMAX, 0, 255).astype(np.uint8)
+            if len(image.shape) != 3:
+                image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+
+            # Convert PointCloud2 message to Open3D PointCloud
+            points = pc2.read_points(pc_msg, field_names=("x", "y", "z"), skip_nans=True)
+            point_cloud = o3d.geometry.PointCloud()
+            point_cloud.points = o3d.utility.Vector3dVector(list(points))
+
+            # Save the data
+            self.poses.append(pose)
+            self.images.append(image)
+            self.point_clouds.append(point_cloud)
+
+            rospy.loginfo('Successfully acquired data')
+        except Exception as ex:
+            rospy.logerr(ex)
+
     def collect_cb(self, _req: TriggerRequest) -> TriggerResponse:
         res = TriggerResponse()
-        rospy.loginfo("Collection triggered...")
 
-        try:
-            if self.last_frame is  None:
-                raise RuntimeError('No image acquired yet')
-            else:
-                diff = rospy.get_time() - self.last_frame.header.stamp.to_sec()
-                if diff > self.sync_time:
-                    raise RuntimeError(f'Last acquired image is {diff - self.sync_time:0.4f} seconds too old')
+        # Call the photoneo GetFrame service
+        photoneo_req = GetFrameRequest()
+        photoneo_req.in_ = -1
 
-                # Convert the image and append
-                image = self.cvb.imgmsg_to_cv2(self.last_frame)
-                if image.dtype != np.dtype(np.uint8):
-                    image = cv2.normalize(image, cv2.NORM_MINMAX, 0, 255).astype(np.uint8)
-                if len(image.shape) != 3:
-                    image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-                self.images.append(image)
+        get_frame = rospy.ServiceProxy('phoxi_camera/get_frame', GetFrame)
+        photoneo_res = get_frame(photoneo_req)
 
-                # Lookup the pose
-                pose = self.buffer.lookup_transform(self.base_frame, self.tool_frame, self.last_frame.header.stamp, rospy.Duration(1))
-                self.poses.append(pose)
-
-                res.success = True
-                res.message = 'Data collected successfully'
-        except Exception as e:
-            res.success = False
-            res.message = f'{e}'
+        res.success = photoneo_res.success
+        if res.success:
+            res.message = "Data capture triggered..."
+        else:
+            res.message = photoneo_res.message
 
         return res
 
-    
     def save_cb(self, _req: TriggerRequest) -> TriggerResponse:
         rospy.loginfo("Save triggered...")
         res = TriggerResponse()
@@ -86,26 +104,31 @@ class DataCollector:
             os.makedirs(image_path, exist_ok=True)
             pose_path = os.path.join(self.parent_path, time_stamp, self.pose_subdir)
             os.makedirs(pose_path, exist_ok=True)
+            cloud_path = os.path.join(self.parent_path, time_stamp, self.cloud_subdir)
+            os.makedirs(cloud_path, exist_ok=True)
 
             cal_data = []
 
             # Write images to pose directory
-            for idx, (img, pose) in enumerate(zip(self.images, self.poses)):
+            for idx, (img, pose, point_cloud) in enumerate(zip(self.images, self.poses, self.point_clouds)):
                 filename = str(idx).zfill(4)
-                image_file = f'{filename}.png'
+                img_file = f'{filename}.png'
                 pose_file = f'{filename}.yaml'
+                cloud_file = f'{filename}.pcd'
                 cal_data.append(
                     {
-                        'image': os.path.join(self.img_subdir, image_file),
-                        'pose': os.path.join(self.pose_subdir, pose_file)
+                        'image': os.path.join(self.img_subdir, img_file),
+                        'pose': os.path.join(self.pose_subdir, pose_file),
+                        'cloud': os.path.join(self.cloud_subdir, cloud_file)
                     }
                 )
 
-                cv2.imwrite(os.path.join(image_path, image_file), img)
+                cv2.imwrite(os.path.join(image_path, img_file), img)
                 self.save_pose(pose, os.path.join(pose_path, pose_file))
+                self.save_point_cloud(point_cloud, os.path.join(cloud_path, cloud_file))
 
             # Write the calibration data file
-            with open(os.path.join(self.parent_path, time_stamp, 'cal_data.yaml'), 'w') as f:
+            with open(os.path.join(self.parent_path, 'cal_data.yaml'), 'w') as f:
                 yaml.dump(cal_data, f)
 
             res.message = f"Saved data due to: \'{self.parent_path}\'"
@@ -134,13 +157,19 @@ class DataCollector:
         with open(filename, 'w') as yaml_file:
             yaml.dump(transform_dict, yaml_file, default_flow_style=False)
 
+    def save_point_cloud(self,
+                         point_cloud: o3d.geometry.PointCloud,
+                         filename: str) -> None:
+        # Save the point cloud as a PCD file
+        o3d.io.write_point_cloud(filename, point_cloud)
+
 
 def main():
-    rospy.init_node("data_collection_node")
+    rospy.init_node("photoneo_data_collection_node")
     _dc = DataCollector()
     rospy.loginfo('Started data collection node...')
     rospy.spin()
 
 
 if __name__ == "__main__":
-    main()   
+    main()

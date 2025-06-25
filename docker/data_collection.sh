47fd7ffb162d3c81deb2dac873fa7486f98ea2f6
Michael Ripperger <michael.ripperger@swri.org>
2025-06-25 18:42:12 -0500

    Added Docker files

---
+++ docker/data_collection.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /opt/industrial_calibration_ros2/setup.bash
+ros2 launch industrial_calibration_ros data_collection.launch.xml \
+  base_frame:=${IC_BASE_FRAME:-base_link} \
+  tool_frame:=${IC_TOOL_FRAME:-tool0} \
+  save_path:=${IC_SAVE_PATH:-/tmp/calibration} \
+  config_file:=${IC_CONFIG_FILE:-/opt/industrial_calibration_ros2/industrial_calibration_ros/share/industrial_calibration_ros/config/target_detector_config.yaml} \
+  sync_time:=${IC_SYNC_TIME:-1.0} \
+  image_topic:=${IC_IMAGE_TOPIC:-/camera/color/image_raw}

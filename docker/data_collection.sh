1ce2f058c953efd04ea2e82501091a670276d853
Michael Ripperger <michael.ripperger@swri.org>
2025-07-29 18:09:04 -0500

    Updated data collection launch files to work for static and moving camera cases

--- docker/data_collection.sh
+++ docker/data_collection.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 source /opt/industrial_calibration_ros2/setup.bash
 ros2 launch industrial_calibration_ros data_collection.launch.xml \
-  base_frame:=${IC_BASE_FRAME:-base_link} \
-  tool_frame:=${IC_TOOL_FRAME:-tool0} \
+  camera_mount_frame:=${IC_CAMERA_MOUNT_FRAME:-tool0} \
+  camera_frame:=${IC_CAMERA_FRAME:-camera} \
+  target_mount_frame:=${IC_TARGET_MOUNT_FRAME:-base_link} \
+  target_frame:=${IC_TARGET_FRAME:-target} \
   save_path:=${IC_SAVE_PATH:-/tmp/calibration} \
   config_file:=${IC_CONFIG_FILE:-/opt/industrial_calibration_ros2/industrial_calibration_ros/share/industrial_calibration_ros/config/target_detector_config.yaml} \
   sync_time:=${IC_SYNC_TIME:-1.0} \
